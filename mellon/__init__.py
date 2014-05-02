199a58d1079f9c04b75d3f0b8c1c445866bab6cc
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-05-02 16:04:31 +0200

    add disclaimer on loading about the need for lasso

--- mellon/__init__.py
+++ mellon/__init__.py
@@ -1,1 +1,7 @@
 __version__ = '1.0.0'
+try:
+    import lasso
+except ImportError:
+    print '***** django-mellon needs the Python binding for the lasso library, *****'
+    print '***** look on http://lasso.entrouvert.org/download/ to obtain it    *****'
+    raise
