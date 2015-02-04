6a6f5e58df4e5f12536b1334343b40f98303d316
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-02-04 12:13:02 +0100

    urls: fix error in pattern, includable patterns must not start with a ^

--- mellon/urls.py
+++ mellon/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-                       url('^login/$', views.login,
+                       url('login/$', views.login,
                            name='mellon_login'),
-                       url('^logout/$', views.logout,
+                       url('logout/$', views.logout,
                            name='mellon_logout'),
-                       url('^metadata/$', views.metadata,
+                       url('metadata/$', views.metadata,
                            name='mellon_metadata'))
