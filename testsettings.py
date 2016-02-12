f2cf86f7a637173499316e212f408a91e8f50a43
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2016-02-12 19:42:48 +0100

    add tox.ini to test on django 1.7, 1.8, 1.9 and with sqlite and pg

--- testsettings.py
+++ testsettings.py
@@ -1,11 +1,12 @@
+import os
 from django.conf import global_settings
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.' + os.environ.get('DB_ENGINE', 'sqlite3'),
         'NAME': 'mellon.sqlite3',
         'TEST': {
-            'NAME': 'mellon-test.sqlite',
+            'NAME': 'mellon-test-' + os.environ.get('DB_ENGINE', 'sqlite3'),
         },
     }
 }
@@ -13,3 +14,4 @@ DEBUG = True
 SECRET_KEY='xx'
 INSTALLED_APPS = ('mellon', 'django.contrib.auth', 'django.contrib.contenttypes', 'django.contrib.sessions')
 MIDDLEWARE_CLASSES = global_settings.MIDDLEWARE_CLASSES
+ROOT_URLCONF = 'mellon.urls'
